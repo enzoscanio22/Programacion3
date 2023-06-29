08138e9428106d707e613c54dd4b940e99797ef8
enzoscanio22 <enzoscanio555@gmail.com>
2023-06-29 18:20:40 -0300

    segundo commit

---
+++ src/Clases/Alumno.tsx
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+class Alumno {
+  nombre: string;
+  apellido: string;
+  constructor(nombre: string, apellido: string) {
+    this.nombre = nombre;
+    this.apellido = apellido;
+  }
+}
+
+export default Alumno;
--- src/index.tsx
+++ src/index.tsx
@@ -1,29 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import CheckBox from './Componentes/Checkbox';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import CheckBox from "./Componentes/Checkbox";
 
-import { BottonGroup } from './Componentes/BottonGroup';
-import { DropDrown } from './Componentes/DropDrown';
-import { InputDate } from './Componentes/InputDate';
-import ToDoList from './Componentes/ToDoList';
-import InputSlider from './Componentes/InputSlider';
-import InputText from './Componentes/InputText';
-import TextAreaExample from './Componentes/InputTextArea';
-import RadioButton from './Componentes/RadioButtom';
-import SwitchButton from './Componentes/SwitchButton';
-
+import { BottonGroup } from "./Componentes/BottonGroup";
+import { DropDrown } from "./Componentes/DropDrown";
+import { InputDate } from "./Componentes/InputDate";
+import ToDoList from "./Componentes/ToDoList";
+import InputSlider from "./Componentes/InputSlider";
+import InputText from "./Componentes/InputText";
+import TextAreaExample from "./Componentes/InputTextArea";
+import RadioButton from "./Componentes/RadioButtom";
+import SwitchButton from "./Componentes/SwitchButton";
+import Automovil from "./Clases/automovil";
+import Escuela from "./Clases/escuela";
+import { CuentaBancaria } from "./Clases/cuentaBancaria";
+import FormasGeometricas from "./Clases/figurasGeometricas";
+// import CuentaBancaria from './Clases/cuentaBancaria';
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 const options = ["Opcion 1", "Opcion 2", "Opcion 3"];
 root.render(
   <React.StrictMode>
-      <div className="App">
-      <h1>TRABAJO PRACTICO N°1</h1>
+    <div className="App">
+      {/* <h1>TRABAJO PRACTICO N°1</h1>
       <CheckBox label="rojo"/>
       <ToDoList/>
       <BottonGroup/>
@@ -33,13 +37,18 @@ root.render(
       <InputText/>
       <TextAreaExample/>
       <RadioButton options={options}/>
-      <SwitchButton/>
-     </div>
+      <SwitchButton/> */}
+      <h1>TRABAJO PRACTICO N°2</h1>
+      <Automovil />
+      <hr></hr>
+      <Escuela />
+      <hr></hr>
+      <CuentaBancaria />
+      <hr></hr>
+      <FormasGeometricas />
+    </div>
     <App />
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
